7d21636e234bb0db61af1a3191e9274876a62666
gittjar <jarno.keranen@gmail.com>
2023-03-09 21:20:10 +0200

    eka update

---
+++ Data/ElokuvaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+public class ElokuvaDBContext : DbContext
+{
+    public ElokuvaDBContext(DbContextOptions<ElokuvaDBContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+}
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+## MVCMovie Webapp
+# MovieAppAzure
